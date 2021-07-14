d1f626df4eca18335843c4c741e87e3fb62e0bdc
Fabian Brash <fabianbrash@gmail.com>
2021-07-14 12:25:57 -0400

    Create map-filter-reduce.py

---
+++ map-filter-reduce.py
@@ -0,0 +1,24 @@
+from functools import reduce
+
+arr = [1,2,3,4]
+# Returns the sum of all the elements using `reduce`
+result = reduce((lambda a, b: a + b), arr)
+print(result)
+
+
+
+
+from functools import reduce
+# Returns the sum of two elements
+def sumTwo(a,b):
+    return a+b
+
+result = reduce(sumTwo, [1, 2, 3, 4])
+print(result)
+
+
+arr2 = [5,6,7,8,9]
+
+data = reduce((lambda a, b: a + b), arr2)
+
+print(data)
