5b06b1a174e01efa81b7f0ee40d3f02297d1f2da
Fabian Brash <fabianbrash@gmail.com>
2021-08-17 10:43:21 -0400

    Create find_folder.py

---
+++ find_folder.py
@@ -0,0 +1,31 @@
+
+folders = [{
+    "folder": "group-v1017",
+    "name": "templates"
+},
+{
+    "folder": "group-v1772",
+    "name": "APPLIANCES"
+},
+{
+    "folder": "group-v2000",
+    "name": "VMs"
+}
+]
+
+#print(data[0]["name"])
+
+for folder in folders:
+    print(f"Folder:{folder['folder]}({folder['name']})")
+
+def getFolderName():
+    theFolder = "group-v1772"
+
+    for f in folders:
+        if f['folder'] == theFolder:
+            print(f"Folder Name is: {f['name']}")
+            return
+    print("Error folder not found...")
+
+
+getFolderName()
