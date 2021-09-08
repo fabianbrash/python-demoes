9a5bcfd0036cf243454a3a8e62fb25701a282223
Fabian Brash <fabianbrash@gmail.com>
2021-09-08 17:51:52 -0400

    added double for loop option

--- bubble.py
+++ bubble.py
@@ -14,3 +14,14 @@ def bubble(list_a):
 
 
 print(bubble([9,3,20,50,6,19,99]))
+
+
+def bubble_for():
+    list = [6,5,4,3,2,1]
+    print(list)
+    for i in range(0, len(list)-1):
+        for j in range(0,len(list)-1 - i):
+            list[j], list[j+1] = list[j+1], list[j]
+    print(list)
+
+bubble_for()
